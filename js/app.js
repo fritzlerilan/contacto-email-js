ad3f87e85cf535fe1076fda48587b467646a7f6c
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-13 02:44:09 -0300

    cursor disabled in send button

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+
+
+eventListeners();
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+}
+
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
