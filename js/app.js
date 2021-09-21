fd8f8ed613a8715bd2940d2bfa4dda5ced641eeb
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-21 09:49:36 -0300

    show error message in validation form

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const btnEnviar = document.querySelector('#enviar');
 const campoEmail = document.querySelector('#email');
 const campoAsunto = document.querySelector('#asunto');
 const campoMensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('#enviar-mail');
+
 
 eventListeners();
 function eventListeners() {
@@ -25,5 +27,20 @@ function validarFormulario(e) {
         console.log('si hay algo');
     }else {
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
     }
+}
+
+function mostrarError(){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+
+    if(!errores.length) {
+        formulario.appendChild(mensajeError);
+    }
+
 }
