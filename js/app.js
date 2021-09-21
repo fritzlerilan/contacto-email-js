83dd5aa73cdcc91ff9b591028d3e35a40227e5cb
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-21 10:13:26 -0300

    field validation

--- js/app.js
+++ js/app.js
@@ -23,23 +23,48 @@ function iniciarApp() {
 }
 
 function validarFormulario(e) {
-    if(e.target.value.length > 0) {
-        console.log('si hay algo');
-    }else {
-        e.target.classList.add('border', 'border-red-500');
 
-        mostrarError();
+    const error = document.querySelector('p.error');
+
+    //Elimina errores
+    if (error) {
+        error.remove();
+    }
+
+    if (e.target.value.length > 0) {
+
+        e.target.classList.remove('border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+    } else {
+        e.target.classList.remove('border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+
+        if (er.test(e.target.value)) {
+            e.target.classList.remove('border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+            console.log('Email valido');
+        } else {
+            e.target.classList.remove('border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            mostrarError('Email no válido');
+        }
     }
 }
 
-function mostrarError(){
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.textContent = mensaje;
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
 
     const errores = document.querySelectorAll('.error');
 
-    if(!errores.length) {
+    if (!errores.length) {
         formulario.appendChild(mensajeError);
     }
 
