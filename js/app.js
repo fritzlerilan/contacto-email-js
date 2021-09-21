c2ec0be4137183f2781d96d804b0afc48448d6d1
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-21 10:41:43 -0300

    final validations for the email sender simulator

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const btnReset = document.querySelector('#resetBtn');
 const campoEmail = document.querySelector('#email');
 const campoAsunto = document.querySelector('#asunto');
 const campoMensaje = document.querySelector('#mensaje');
 const formulario = document.querySelector('#enviar-mail');
+
+const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
 eventListeners();
@@ -15,6 +18,12 @@ function eventListeners() {
     campoEmail.addEventListener('blur', validarFormulario);
     campoAsunto.addEventListener('blur', validarFormulario);
     campoMensaje.addEventListener('blur', validarFormulario);
+
+    // Enviar email
+    formulario.addEventListener('submit', enviarEmail);
+
+    // Reinicia el formulario 
+    btnReset.addEventListener('click', limpiarFormulario);
 }
 
 function iniciarApp() {
@@ -24,15 +33,11 @@ function iniciarApp() {
 
 function validarFormulario(e) {
 
-    const error = document.querySelector('p.error');
-
-    //Elimina errores
-    if (error) {
-        error.remove();
-    }
-
     if (e.target.value.length > 0) {
-
+        const error = document.querySelector('p.error');
+        if (error) {
+            error.remove();
+        }
         e.target.classList.remove('border-red-500');
         e.target.classList.add('border', 'border-green-500');
     } else {
@@ -42,10 +47,13 @@ function validarFormulario(e) {
     }
 
     if (e.target.type === 'email') {
-        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
-        if (er.test(e.target.value)) {
+        if (email_regex.test(e.target.value)) {
+            const error = document.querySelector('p.error');
+            if (error) {
+                error.remove();
+            }
             e.target.classList.remove('border-red-500');
             e.target.classList.add('border', 'border-green-500');
             console.log('Email valido');
@@ -55,7 +63,16 @@ function validarFormulario(e) {
             mostrarError('Email no válido');
         }
     }
+    if (email_regex.test(campoEmail.value) && campoAsunto.value !== '' && mensaje.value !== '') {
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
+    } else {
+        iniciarApp();
+    }
 }
+
+
+
 
 function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
@@ -68,4 +85,30 @@ function mostrarError(mensaje) {
         formulario.appendChild(mensajeError);
     }
 
+}
+
+function enviarEmail(e) {
+    e.preventDefault();
+    // Mostrar spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+
+    // Despues de 3 segundos ocultar el spinner y mostrar el mensaje
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        const parrafo = document.createElement('p');
+        parrafo.textContent = 'El mensaje se envio correctamente';
+        parrafo.classList.add('text-center', 'mt-10', 'p-5', 'bg-green-500', 'text-white', 'uppercase', 'mb-10');
+        formulario.insertBefore(parrafo, spinner);
+
+        setTimeout(() => {
+            parrafo.remove();
+            limpiarFormulario();
+            iniciarApp();
+        }, 5000);
+    }, 3000);
+}
+
+function limpiarFormulario() {
+    formulario.reset();
 }
